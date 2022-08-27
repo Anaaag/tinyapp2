aa1fb2ea34a600c695bf824d53dcb2bde49096c5
Anaaag <agarc20@outlook.com>
2022-08-26 20:26:34 -0700

    add post route that removes url

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true}));
+// app.use(express.urlencoded({ extended: true}));
 
 function generateRandomString() {
   return Math.random().toString(23).slice(8);
@@ -16,6 +16,12 @@ const urlDatabase = {
 };
 
 const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -26,7 +32,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase)
+  res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
@@ -34,24 +40,27 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longUrl: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-})
-
+});
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString()
-  urlDatabase[shortURL] = req.body.longUrl
-  console.log(req.body);
-  res.redirect(`/urls/${shortURL}`)
+  urlDatabase[shortURL] = req.body.longURL
+  console.log(req.body);  
+  res.redirect(`/urls/${shortURL}`)        
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longUrl = urlDatabase[req.params.shortURL]
-  res.redirect(longUrl);
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
-
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL
+  delete urlDatabase[shortURL] 
+  res.redirect("/urls")
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
